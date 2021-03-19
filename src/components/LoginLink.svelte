ec6b58d3cf3d6df24de92e828ad92cc006753ee4
anfieldfoot <48527670+anfieldfoot@users.noreply.github.com>
2021-03-19 17:12:08 +0000

    First commit

---
+++ src/components/LoginLink.svelte
@@ -0,0 +1,23 @@
+<script>
+  import user from "../stores/user.js";
+  import { link } from "svelte-routing";
+  // logout func
+  import globalStore from "../stores/globalStore.js";
+</script>
+
+{#if $user.jwt}
+  <a
+    href="/"
+    class="logout-btn"
+    on:click={() => {
+      globalStore.toggleItem("sidebar", false);
+    }}>logout</a
+  >
+{:else}
+  <a
+    href="/login"
+    on:click={() => {
+      globalStore.toggleItem("sidebar", false);
+    }}>login</a
+  >
+{/if}
---
+++ src/components/Products/Product.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let product;
+  const { title, image, price, id } = product;
+  import { link } from "svelte-routing";
+  import { text } from "svelte/internal";
+</script>
+
+<article class="product">
+  <div class="img-container">
+    <img src={image} alt={title} />
+    <a href={`/products/${id}`} class="btn btn-primary product-link" use:link
+      >details</a
+    >
+  </div>
+  <div class="product-footer">
+    <p class="product-title">{title}</p>
+    <p class="product-price">£{price}</p>
+  </div>
+</article>
